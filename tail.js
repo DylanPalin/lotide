97b58bc6efb1851f77bcabca5a42c5581ace9d86
Dylan Palin <dylanpalin@gmail.com>
2023-11-14 14:51:38 -0700

    removed array values from test and defines them in variable, then calls them from the test case

--- tail.js
+++ tail.js
@@ -7,9 +7,17 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// Create a function tail which returns the "tail" 
+// of an array: everything except for the first item (head) of the provided array.
+
 const tail = function(array) {
   return array.slice(1);
 };
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+const result1 = tail(["Hello", "Lighthouse", "Labs", "Testing"]);
+const result2 = tail([])
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+assertEqual(result.length, 2);
+assertEqual(result1.length, 3);
+assertEqual(result2.length, 0)
