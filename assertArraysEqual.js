b03bd25f5ba3a56f2fca4143c09fddede0138a1e
Dylan Palin <dylanpalin@gmail.com>
2023-11-15 15:40:35 -0700

    fixed known errors

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,29 @@
-const assertArraysEqual = function(a, b) {
-  if (a.length !== b.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
-    return;
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return true;
+  } else if (actual !== expected) {
+    return false;
   }
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
-    return;
-    }
-  }
-  console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
 };
-
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+const assertArraysEqual = function(a, b) {
+    if (eqArrays(a, b)) {
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+    } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+  }
+};
 // TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
