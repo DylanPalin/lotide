deba3fbfafc31a9804928c55930751632740f537
Dylan Palin <dylanpalin@gmail.com>
2023-11-16 00:05:46 -0700

    edited three times

--- without.js
+++ without.js
@@ -28,8 +28,8 @@ const without = function(source, itemsToRemove) {
 }
 
 // TEST CODE
-assertArraysEqual(without([1, 2, 3], [1])); // => [2, 3]
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+assertArraysEqual(without([1, 2, 3], [1], [2, 3]));
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"], ["1", "2"]));
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
