56a3f2b6c1d1f942ff0dddaa167290936309783e
Dylan Palin <dylanpalin@gmail.com>
2023-11-15 23:59:55 -0700

    fixed errros

--- without.js
+++ without.js
@@ -12,9 +12,9 @@ const eqArrays = function(a, b) {
 };
 const assertArraysEqual = function(a, b) {
   if (eqArrays(a, b)) {
-    return true;
+    return (console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`));
   } else {
-    return false;
+    return (console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`)));
   }
 };
 const without = function(source, itemsToRemove) {
