90aeb6b633ba387ef217e3717bda610d56c1adfa
Dylan Palin <dylanpalin@gmail.com>
2023-11-15 20:36:14 -0700

    fiixed

--- middle.js
+++ middle.js
@@ -30,9 +30,9 @@ const middle = function(array) {
   return middleNum;
 };
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+assertArraysEqual(middle([1])); 
+assertArraysEqual(middle([1, 2])); 
+assertArraysEqual(middle([1, 2, 3]), 2); 
+assertArraysEqual(middle([1, 2, 3, 4, 5]), 3); 
+assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3])); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), ([3, 4])); 
