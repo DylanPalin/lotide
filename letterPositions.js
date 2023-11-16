d5289717d7cf4228daf735461edc5653ec5eafc3
Dylan Palin <dylanpalin@gmail.com>
2023-11-16 13:47:48 -0700

    finished assignment letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,35 @@
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a, b)) {
+    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${a} !== ${b}`);
+  }
+};
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letter = sentence[i];
+    if (letter !== " " && letter !== "," && letter !== ".") {
+      if (results[letter]) {
+        results[letter].push(i);
+      } else {
+        results[letter] = [i];
+      }
+    }
+  }
+  return results;
+};
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
