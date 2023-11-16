b6acc7d9fbb9a75c517e426091035c9a68307d85
Dylan Palin <dylanpalin@gmail.com>
2023-11-16 15:55:43 -0700

    completed eqObjects

---
+++ eqObjects.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1).sort();
+  const keys2 = Object.keys(object2).sort();
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let i = 0; i < keys1.length; i++) {
+    if (keys1[i] !== keys2[i] || object1[keys1[i]] !== object2[keys2[i]]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject);
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);
