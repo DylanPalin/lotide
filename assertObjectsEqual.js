3b142069667df3e1253dd8c34cf1b8a976875404
Dylan Palin <dylanpalin@gmail.com>
2023-11-16 17:36:32 -0700

    fixed errors

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,15 @@
+const eqArrays = function(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  } else {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== b[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1).sort();
   const keys2 = Object.keys(object2).sort();
@@ -8,10 +20,16 @@ const eqObjects = function(object1, object2) {
     if (keys1[i] !== keys2[i]) {
       return false;
     }
+    if (Array.isArray(object1[keys1[i]]) && Array.isArray(object2[keys2[i]])) {
+      if (!eqArrays(object1[keys1[i]], object2[keys2[i]])) {
+        return false;
+      }
+    } else if (object1[keys1[i]] !== object2[keys2[i]]) {
+      return false;
+    }
   }
   return true;
 };
-
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect; // <= add this line
