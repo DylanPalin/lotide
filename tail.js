c1c5e9eb8246467b291aa95e7dbf9bc90a4d2711
Dylan Palin <dylanpalin@gmail.com>
2023-11-20 16:52:58 -0700

    exported tail function

--- tail.js
+++ tail.js
@@ -1,15 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// Create a function tail which returns the "tail"
-// of an array: everything except for the first item (head) of the provided array.
-
 const tail = function(array) {
   return array.slice(1);
 };
@@ -19,9 +7,4 @@ tail(words);
 const result2 = tail(["Hello"]);
 const result3 = tail([]);
 
-// TEST CODE
-assertEqual(result[0], "Lighthouse");
-assertEqual(result.length, 2);
-assertEqual(words.length, 3);
-assertEqual(result2.length, 0);
-assertEqual(result3.length, 0);
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual(result[0], "Lighthouse");
+assertEqual(result.length, 2);
+assertEqual(words.length, 3);
+assertEqual(result2.length, 0);
+assertEqual(result3.length, 0);
