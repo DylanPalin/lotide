11b3d102083e1b7af79e6a44049c20af32779b04
Dylan Palin <dylanpalin@gmail.com>
2023-11-20 20:39:45 -0700

    refactored middle and fixed test cases

--- middle.js
+++ middle.js
@@ -1,23 +1,7 @@
-const eqArrays = function(a, b) {
-  if (a.length !== b.length) {
-    return false;
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-const assertArraysEqual = function(a, b) {
-  if (eqArrays(a, b)) {
-    return console.log(`${a} === ${b}`);
-  } else {
-    return console.log(`${a} !== ${b}`);
-  }
-};
 const middle = function(array) {
+  if (!array) {
+    return [];
+}
   let middleNum = [];
   let midIndex = Math.floor(array.length / 2);
   if (array.length < 3) {
@@ -30,9 +14,4 @@ const middle = function(array) {
   return middleNum;
 };
 
-assertArraysEqual(middle([1]));
-assertArraysEqual(middle([1, 2]));
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]));
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), ([3, 4]));
+module.exports = middle;
