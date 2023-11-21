58bd11ec73d810179c0d87fb3be9331597be0712
Dylan Palin <dylanpalin@gmail.com>
2023-11-20 21:01:52 -0700

    commented out removed

--- test/headTest.js
+++ test/headTest.js
@@ -9,8 +9,3 @@ describe("#head", () => {
     assert.strictEqual(head(['5']), '5'); 
   });
 });
-
-
-// TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
